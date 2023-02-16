9522237bc317a08be4c02882fa873d7e7234090f
raisiali2 <aliraisi807@gmail.com>
2023-02-16 18:43:49 +0100

    vodio 1.2.1-intoduction to docker

---
+++ data-engineering/week_1_basics_n_setup/2_docker_sql/pipeline.py
@@ -0,0 +1,19 @@
+# this is our data pipeline and in this data pipeline we use pandas
+
+import sys
+
+import pandas as pd
+
+print("what will be sys arg",sys.argv)
+
+day = sys.argv[1]
+  
+# some practice with pandas
+# 1
+# print("job finished successfully")
+
+# now we copy this file from current directory to docker image
+# we can also specify the working directory
+
+# 2
+print(f"job finished successfully for day = {day}")
