b59e74a644123c91d2d2e0bf15d22f6f29f4d4cb
raisiali2 <aliraisi807@gmail.com>
2023-02-28 18:44:04 +0100

    create aws s3 resources and iam policy for that with terroform

--- data-engineering/week_1_basics_n_setup/1_terraform_aws/terraform/s3/main.tf
+++ data-engineering/week_1_basics_n_setup/1_terraform_aws/terraform/s3/main.tf
@@ -1,1 +1,79 @@
- 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_s3_bucket" "Bucket1s3" {
+  bucket = var.bucket_name_data
+  acl    = "private"
+  tags = {
+    Name = "created using terraform"
+  }
+
+}
+
+resource "aws_s3_bucket" "Bucket2s3" {
+  bucket = var.bucket_name_dag
+  acl    = "private"
+  tags = {
+    Name = "created using terraform"
+  }
+
+}
+
+
+resource "aws_iam_policy" "rw_policy_bucket" {
+  name        = "s3_rw_policy_zoomcamp"
+  path        = "/"
+  description = "Allow rw access to S3"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:DeleteObject"
+        ],
+        "Resource" : [
+          "arn:aws:s3:::*/*"
+        ]
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_policy" "read_policy_bucket" {
+  name        = "s3_read_policy_zoomcamps"
+  path        = "/"
+  description = "Allow read access to s3 "
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : ["s3:GetObject",
+          "s3:ListBucket"
+        ],
+        "Resource" : [
+          "arn:aws:s3:::*/*"
+        ]
+      }
+    ]
+  })
+}
+
--- data-engineering/week_1_basics_n_setup/1_terraform_aws/terraform/s3/variables.tf
+++ data-engineering/week_1_basics_n_setup/1_terraform_aws/terraform/s3/variables.tf
@@ -1,1 +1,12 @@
- 
+variable "bucket_name_data" {
+  description = "zoomcamp data"
+  type        = string
+  default     = "zoomcamp-data-tf3bucket"
+}
+
+variable "bucket_name_dag" {
+  description = "zoomcamp dags"
+  type        = string
+  default     = "zoomcamp-airflowdags-tf3bucket"
+}
+
