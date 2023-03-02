504462bc58299b1cc720bb35ebd710bf1def9714
raisiali2 <aliraisi807@gmail.com>
2023-03-02 22:49:13 +0100

    psycopg2 package added

--- data-engineering/week_1_basics_n_setup/1_terraform_aws/terraform/lambda/LoadToRedshift/psycopg2/_lru_cache.py
+++
@@ -1,5 +0,0 @@
-"""
-LRU cache implementation for Python 2.7
-Ported from http://code.activestate.com/recipes/578078/ and simplified for our
-use (only support maxsize > 0 and positional arguments).
-"""
